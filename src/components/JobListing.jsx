86a7a7707982c271b3978ebdf611593c4c5f0fd2
cloudgustPro <cloudgust.pro@gmail.com>
2024-07-21 20:50:31 +0300

    json server

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import job from '../jobs.json';
+import { useState, useEffect } from 'react';
 import JobList from './JobList'
 const JobListing = ({isHome = false}) => {
+const [Job, setJob] = useState([])
+const [Loading, setLoading] = useState(true)
 
-const recentJob= isHome ? job.slice(0,3) : job
+
+useEffect(
+  ()=>{
+    const fetchJobs = async()=>{
+      try {
+        const res = await fetch('https://localhost:3000/jobs')
+        const data = await res.json()
+        setJob(data)
+      } catch (error) {
+        console.log('error fetching darta', error)
+      }finally{
+        setLoading(false)
+      }
+    }
+    fetchJobs();
+  },
+   []
+)
+
   return (
-
       <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
@@ -13,13 +32,14 @@ const recentJob= isHome ? job.slice(0,3) : job
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-            {recentJob.map((job)=>(
+            {Job.map((job)=>(
                 <JobList job = {job} key = {job.id} /> 
             ))}
         </div>
       </div>
     </section>
   )
+
 }
 
 export default JobListing
